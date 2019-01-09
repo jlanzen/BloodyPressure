1ccac4bb09f901729f8488680931402e742dfe9e
Your Name <you@example.com>
2019-01-09 08:41:58 +0100

    added a simple graph to visualize values

--- app/src/main/java/com/joe/kotlinfromthescratch/activities/GraphActivity.kt
+++ app/src/main/java/com/joe/kotlinfromthescratch/activities/GraphActivity.kt
@@ -1,35 +1,66 @@
 package com.joe.kotlinfromthescratch.activities
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.View
+import bloodyPressure.BloodPressure
+import bloodyPressure.BloodPressureDao
+import bloodyPressure.BloodyDatabase
 import com.joe.kotlinfromthescratch.R
 import kotlinx.android.synthetic.main.bottom_navigation_layout.*
 import com.jjoe64.graphview.GraphView
 import com.jjoe64.graphview.series.DataPoint
 import com.jjoe64.graphview.series.LineGraphSeries
+import com.joe.kotlinfromthescratch.model.Entry
+import org.jetbrains.anko.doAsync
 
 
 class GraphActivity : AppCompatActivity() {
+
+    private var db: BloodyDatabase? = null
+    private var bloodPressureDao: BloodPressureDao? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.graph_layout)
 
-        val graph = findViewById<View>(R.id.graph) as GraphView
-        val series = LineGraphSeries(
-            arrayOf<DataPoint>(
-                DataPoint(0.0, 1.0),
-                DataPoint(1.0, 5.0),
-                DataPoint(2.0, 3.0),
-                DataPoint(3.0, 2.0),
-                DataPoint(4.0, 6.0)
-            )
-        )
-        graph.addSeries(series)
+        db = BloodyDatabase.getAppDataBase(context = this)
+        bloodPressureDao = db?.bloodPressureDao()
+
+        doAsync {
+            val allItems = db?.bloodPressureDao()?.getAllByTimestamp()
+            var systolicArrayList : ArrayList<DataPoint> = ArrayList<DataPoint>()
+            var diastolicArrayList : ArrayList<DataPoint> = ArrayList<DataPoint>()
+            var pulseArrayList : ArrayList<DataPoint> = ArrayList<DataPoint>()
+            if (allItems != null) {
+                var i : Int = 0
+                for (bp : BloodPressure in allItems) {
+                    systolicArrayList.add(DataPoint(i.toDouble(), bp.systolic.toDouble()))
+                    diastolicArrayList.add(DataPoint(i.toDouble(), bp.diastolic.toDouble()))
+                    pulseArrayList.add(DataPoint(i.toDouble(), bp.pulse.toDouble()))
+                    i++
+                }
+            }
+            val graph = findViewById<View>(R.id.graph) as GraphView
+            var systolicSeries : LineGraphSeries<DataPoint> = LineGraphSeries(systolicArrayList.toTypedArray())
+            systolicSeries.color = Color.GREEN
+            systolicSeries.backgroundColor = Color.TRANSPARENT
+            var diastolicSeries : LineGraphSeries<DataPoint> = LineGraphSeries(diastolicArrayList.toTypedArray())
+            diastolicSeries.color = Color.RED
+            diastolicSeries.backgroundColor = Color.TRANSPARENT
+            var pulseSeries : LineGraphSeries<DataPoint> = LineGraphSeries(pulseArrayList.toTypedArray())
+            pulseSeries.color = Color.BLUE
+            pulseSeries.backgroundColor = Color.TRANSPARENT
+
+            graph.addSeries(systolicSeries)
+            graph.addSeries(diastolicSeries)
+            graph.addSeries(pulseSeries)
+
+        }
 
         val bottomNavigation: BottomNavigationView = findViewById(R.id.bottom_navigation)
         bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
